e62222f0c39b048ac674824765506f4513d05d68
Richmond Wong <richmondwong@gmail.com>
2018-12-03 02:17:08 +0000

    Get Monty Python script

--- http_client_streams.js
+++ http_client_streams.js
@@ -1,8 +1,8 @@
 var https = require('https');
 
 var options = {
-  host: 'www.nytimes.com',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
